6a16a3171aa228c25c0cff348a74812d1d787da6
Jose Orlando da Silva <122377950+orlandosilva92@users.noreply.github.com>
2025-09-15 22:59:24 -0300

    Update switchCase.cpp

--- Aula 4/switchCase.cpp
+++ Aula 4/switchCase.cpp
@@ -5,8 +5,43 @@ using namespace std;
 int main(){
 	setlocale(LC_ALL,"Portuguese");
 	
+	int opcao;
 	
+	cout << "Escolha o númeor do pais que vai ganhar a copa do mundo:\n\n";
+	cout << "1 - Espanha\n";
+	cout << "2 - Alemanha\n";	
+	cout << "3 - Franca\n";
+	cout << "4 - Brasil\n";
+	cout << "5 - Italia\n";
+	cout << "6 - Holanda\n";
 	
+	cin >> opcao;
+	
+	switch (opcao){	
+	
+	case 1:
+		cout << "O país que irá ganhar a proxima copa é: Espanha\n";
+	break; 
+	case 2:
+		cout << "O país que irá ganhar a proxima copa é: Alemanha\n";
+	break; 
+	case 3:
+		cout << "O país que irá ganhar a proxima copa é: Franca\n";
+	break; 
+	case 4:
+		cout << "O país que irá ganhar a proxima copa é: Brasil\n";
+	break; 
+	case 5:
+		cout << "O país que irá ganhar a proxima copa é: Italia\n";
+	break; 
+	case 6:
+		cout << "O país que irá ganhar a proxima copa é: Holanda\n";
+	break; 
+	default:
+		cout << "Valor incorreto!\n";
+	break;
+	}
+		
 	system("pause");
 	return 0;
 }
