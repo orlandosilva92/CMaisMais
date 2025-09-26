acf49e4f14b0cb363468615d54220b73a1484633
Jose Orlando da Silva <122377950+orlandosilva92@users.noreply.github.com>
2025-09-25 23:57:16 -0300

    ajustes

--- Aula 4/whileDigitarZeroSai.cpp
+++ Aula 4/whileDigitarZeroSai.cpp
@@ -10,22 +10,29 @@ int main(){
 	e só saia da rotina quando o usuário digitar o valor 0(zero)
 	caso o usuário não digite o valor 0(zero) o algoritmo solicitara
 	a entrada de um novo número
+	
+	Após o usuário digitar o valor zero(0) o algoritmo deverá imprimir
+	a quantidade de numeros que foram digitados e a média destes números
 	*/
 	
-	int contador = 1;
+	int contador = 1,soma = 0,media,quantidade = 0;
 	
 	while (contador > 0){
 		
 		cout << "Digite um número\n";
 		cin >> contador;
 		cout << "Número digitado: " << contador << "\n";
+		soma += contador;//aqui vai somando todos os números que já foram digitados
+		quantidade++;//aqui é possivel contar quantos números foram digitados, pois toda vez que é digitado um número é incrementado +1
 		
 			if(contador == 0){
 				break;
 			}
 			
 	}
-	
+	media = soma/quantidade;
+	cout << "Foram digitados " << quantidade << " números.\n";
+	cout << "A média desses números é: " << media << ".\n";
 	cout << "\nVocê digitou o número zero (0). \nPrograma encerrado.\n";
 	
 	system("pause");
