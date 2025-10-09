0b8fcef2b3399131341141fcb47887c775637cdd
Jose Orlando da Silva <122377950+orlandosilva92@users.noreply.github.com>
2025-10-08 22:57:42 -0300

    Update for.cpp

--- Aula 5/for.cpp
+++ Aula 5/for.cpp
@@ -5,7 +5,15 @@ using namespace std;
 int main(){
 	setlocale(LC_ALL,"Portuguese");
 	
+	int numero;
 	
+	//for = para
+	//(condição inicial;teste;incremento ou decremento)
+	for(numero = 1;numero < 11;numero++){
+		
+		cout << numero << "\n";
+		
+	}
 	
 	system("pause");
 	return 0;
